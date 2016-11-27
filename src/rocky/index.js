1ba1771bb878179985326d558a4fda49eaf8e225
Alexander Koziol <alekoz47@gmail.com>
2016-11-27 00:21:44 -0500

    add drawTicks

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -6,12 +6,25 @@ var rocky = require("rocky");
 //================================
 //Functions:
 
-function drawBars(ctx, hs, w, h) {
+function drawBars(ctx, hs, w, h, minute, hour, day) {
 	var x = w / 4;
 	var y = hs - (7 * h);
-	for (var ii = 0; ii < 5; ii++) {
+	
+	for (var ii = 0; ii <= 5; ii++) {
 		if (ii % 2 === 0) {
 			drawBar(ctx, x, y, "red", w, h);
+			switch (ii / 2) {
+				case 0:
+					drawBar(ctx, x + 3, y + 3, "black", day, h - 6);
+					break;
+				case 1:
+					drawBar(ctx, x + 3, y + 3, "black", hour, h - 6);
+					break;
+				case 2:
+					drawBar(ctx, x + 3, y + 3, "black", minute, h - 6);
+					break;
+			}
+			drawTicks(ctx, w, h, x, y + h, ii);
 		} else {
 			drawBar(ctx, x, y, "white", w, h);
 		}
@@ -24,6 +37,35 @@ function drawBar(ctx, x, y, color, wb, hb) {
 	ctx.fillRect(x, y, wb, hb);
 }
 
+function drawTicks(ctx, w, h, x, y, index) {
+	var tickCount = 0;
+	ctx.lineWidth = 3;
+	ctx.strokeStyle = "darkgrey";
+	switch (index) {
+		case 0:
+			tickCount = 30;
+			break;
+		case 1:
+			tickCount = 24;
+			break;
+		case 2:
+			tickCount = 60;
+			break;
+	}
+	var tickAdvance = w / tickCount;
+	
+	for (var ii = x + 3; ii <= tickCount * tickAdvance; ii += tickAdvance) {
+		drawLine(ctx, ii, y, ii, y - (h / 3));
+	}
+}
+
+function drawLine(ctx, x1, y1, x2, y2) {
+	ctx.beginPath();
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+	ctx.stroke();
+}
+
 //================================
 //Run:
 
@@ -34,18 +76,15 @@ rocky.on("draw", function(event) {
 	var h = ctx.canvas.unobstructedHeight;
 	var barWidth = (w / 2) + 30;
 	var barHeight = (h / 9);
-	var minuteLength = (d.getMinutes() / 60) * barWidth;
-	var hourLength = (d.getHours() / 24) * barWidth;
-	var dayLength = (d.getDate() / 30) * barWidth;
+	var minuteLength = (d.getMinutes() / 60) * (barWidth - 6);
+	var hourLength = (d.getHours() / 24) * (barWidth - 6);
+	var dayLength = (d.getDate() / 30) * (barWidth - 6);
 	
 	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, w, h);
 	
-	drawBars(ctx, h, barWidth, barHeight);
-	drawBar(ctx, barWidth / 4, h - (7 * barHeight) + 3, "black", minuteLength, 14);
-	drawBar(ctx, barWidth / 4, h - (5 * barHeight) + 3, "black", hourLength, 14);
-	drawBar(ctx, barWidth / 4, h - (3 * barHeight) + 3, "black", dayLength, 14);
+	drawBars(ctx, h, barWidth, barHeight, minuteLength, hourLength, dayLength);
 });
 
 rocky.on("minutechange", function(event) {
