cc1a7f98689020cf1e6fb4ed75c3751cd98c10e0
Alexander Koziol <alekoz47@gmail.com>
2016-11-26 21:56:39 -0500

    add bars

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -2,32 +2,30 @@
 //Require:
 
 var rocky = require("rocky");
-var barWidth = 100;
-var barHeight = 20;
 
 //================================
 //Functions:
 
-function drawBars(ctx, w, h) {
-	var y = 20;
-	for (var ii = 0; ii <= 6; ii++) {
+function drawBars(ctx, hs, w, h) {
+	var x = w / 4;
+	var y = hs - (7 * h);
+	for (var ii = 0; ii < 5; ii++) {
 		if (ii % 2 === 0) {
-			drawBar();
-			y += barHeight;
+			drawBar(ctx, x, y, "red", w, h);
 		} else {
-			drawBar();
-			y += barHeight;
+			drawBar(ctx, x, y, "white", w, h);
 		}
+		y += h;
 	}
 }
 
-function drawBar(ctx, y, color) {
-	ctx.fillStyle = color;
-	ctx.fillRect(20, y, barWidth, barHeight);
+function drawLoads(ctx, ws, hs, wb, hb, days, minutes, hours) {
+	return null;
 }
 
-function drawSlides() {
-	return null;
+function drawBar(ctx, x, y, color, wb, hb) {
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, wb, hb);
 }
 
 //================================
@@ -38,15 +36,18 @@ rocky.on("draw", function(event) {
 	var d = new Date();
 	var w = ctx.canvas.unobstructedWidth;
 	var h = ctx.canvas.unobstructedHeight;
+	var barWidth = (w / 2) + 30;
+	var barHeight = (h / 9);
 	var minutes = (d.getMinutes()) / 60;
 	var hours = (d.getHours() % 12 + (minutes / 60)) / 12;
+	var days = d.getDate();
 	
 	ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, w, h);
 	
-	drawBars(ctx, w, h);
-	drawSlides(ctx, minutes, hours);
+	drawBars(ctx, h, barWidth, barHeight);
+	drawLoads(ctx, h, barWidth, barHeight - 5, days, hours, minutes);
 });
 
 rocky.on("minutechange", function(event) {
